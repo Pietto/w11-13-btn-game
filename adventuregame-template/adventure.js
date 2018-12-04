74592b5b4a4366540462005b4a5a9f2a6b1e0c4b
Pieterjan van Dijk <Pieterjan.v.dijk@gmail.com>
2018-12-04 12:34:36 +0100

    .

--- adventuregame-template/adventure.js
+++ adventuregame-template/adventure.js
@@ -1,5 +1,5 @@
 var delay1 = 0
-var delay1time = 100
+var delay1time = 1500
 
 var originaldelay = delay1
 var originaldelaytime = delay1time
@@ -9,7 +9,7 @@ var button2 = document.getElementById("button2");
 var button3 = document.getElementById("button3");
 
 button1.onclick = s1button1clicked;
-button2.onclick = s1button2clicked;
+button2.onclick = introlevel;
 button3.onclick = s1button3clicked;
 
 
@@ -21,10 +21,10 @@ function s1button1clicked(){
 
 }
 
-function s1button2clicked(){
+function introlevel(){
 	document.getElementById('button2').style.visibility = 'hidden';
 	document.getElementById("game-container").style.background = "url('img/scenes/scene-1/arrest-1.PNG')";
-	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-1/arrest-1.PNG')";}, delay1+=delay1time);
+	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-1/arrest-1.PNG')";}, delay1);
 	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-1/arrest-2.PNG')";}, delay1+=delay1time);
 	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-1/arrest-3.PNG')";}, delay1+=delay1time);
 	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-1/arrest-4.PNG')";}, delay1+=delay1time);
@@ -36,7 +36,7 @@ function s1button2clicked(){
 
 	document.getElementById('button2').id= 'button2pt2';
 
-	button2pt2.onclick = s2button2clicked;
+	button2pt2.onclick = courtlevel;
 
 	setTimeout(function() {document.getElementById('button2pt2').style.visibility = 'visible';}, delay1+=delay1time);
 
@@ -45,20 +45,20 @@ function s1button2clicked(){
 function s1button3clicked() {
 
 }
-function s2button2clicked() {
+function courtlevel() {
 	delay2 = originaldelay
 	delay2time = originaldelaytime
 	document.getElementById('button2pt2').style.visibility = 'hidden';
-	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-2/court-1.PNG')";}, delay2+=delay2time);
+	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-2/court-1.PNG')";}, delay2);
 	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-2/court-2.PNG')";}, delay2+=delay2time);
 
 	document.getElementById('button1').id= 'button1pt2';
 	document.getElementById('button2pt2').id= 'button2pt02';
 	document.getElementById('button3').id= 'button3pt2';
 
-	setTimeout(function() {button1pt2.onclick = s02button1clicked;}, delay2+=delay2time);
-	setTimeout(function() {button2pt02.onclick = s02button2clicked;}, delay2+=delay2time);
-	setTimeout(function() {button3pt2.onclick = s02button3clicked;}, delay2+=delay2time);
+	setTimeout(function() {button1pt2.onclick = goprison;}, delay2+=delay2time);
+	setTimeout(function() {button2pt02.onclick = stillgoprison;}, delay2+=delay2time);
+	setTimeout(function() {button3pt2.onclick = goshooting;}, delay2+=delay2time);
 
 	setTimeout(function() {document.getElementById('button1pt2').style.visibility = 'visible';}, delay2-=delay2time);
 	setTimeout(function() {document.getElementById('button2pt02').style.visibility = 'visible';}, delay2);
@@ -66,26 +66,41 @@ function s2button2clicked() {
 }
 
 
-function s02button1clicked() {
+function goprison() {
 	delay3 = originaldelay
 	delay3time = originaldelaytime
-	alert("wrong button son!")
+
+	document.getElementById('button1pt2').style.visibility = 'hidden';
+	document.getElementById('button2pt02').style.visibility = 'hidden';
+	document.getElementById('button3pt2').style.visibility = 'hidden';
+	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-2/court-choice-prison.png')";}, 0);
+	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-2/court-choice-prison-2.png')";}, delay3+=delay3time);
+
+	setTimeout(function() {prisonlevel();}, delay3+=delay3time);
 }
-function s02button2clicked() {
+function stillgoprison() {
 	delay3 = originaldelay
 	delay3time = originaldelaytime
-	alert("wrong button son!")
+
+	document.getElementById('button1pt2').style.visibility = 'hidden';
+	document.getElementById('button2pt02').style.visibility = 'hidden';
+	document.getElementById('button3pt2').style.visibility = 'hidden';
+
+	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-2/court-choice-stillprison.png')";}, 0);
+	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-2/court-choice-stillprison-1.png')";}, delay3+=delay3time);
+
+	setTimeout(function() {prisonlevel();}, delay3+=delay3time);
 }
-function s02button3clicked() {
+function goshooting() {
 
 	delay3 = originaldelay
 	delay3time = originaldelaytime
 
-	setTimeout(function() {document.getElementById('button1pt2').style.visibility = 'hidden';}, delay3+=delay3time);
-	setTimeout(function() {document.getElementById('button2pt02').style.visibility = 'hidden';}, delay3);
-	setTimeout(function() {document.getElementById('button3pt2').style.visibility = 'hidden';}, delay3);
+	document.getElementById('button1pt2').style.visibility = 'hidden';
+	document.getElementById('button2pt02').style.visibility = 'hidden';
+	document.getElementById('button3pt2').style.visibility = 'hidden';
 
-	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-2/court-choice-fight-1.png')";}, delay3+=delay3time);
+	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-2/court-choice-fight-1.png')";}, 0);
 	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-2/court-choice-fight-2.png')";}, delay3+=delay3time);
 	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-2/court-choice-fight-3.png')";}, delay3+=delay3time);	
 	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-2/court-choice-fight-4.png')";}, delay3+=delay3time);
@@ -94,8 +109,45 @@ function s02button3clicked() {
 	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-2/court-choice-fight-6.5.png')";}, delay3+=delay3time);
 	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-2/court-choice-fight-7.png')";}, delay3+=delay3time);
 	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-2/court-choice-fight-8.png')";}, delay3+=delay3time);
-	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/defeat/defeat 1.0.png')";}, delay3+=(delay3time*2));
-	setTimeout(function() {document.getElementById('button2pt02').style.visibility = 'visible';}, delay3);
+	setTimeout(function() {defeat();}, delay3+=(delay3time*2));
+}
+
+function prisonlevel() {
+	delay4 = originaldelay
+	delay4time = originaldelaytime
+	document.getElementById("game-container").style.background = "url('img/scenes/scene-3/prison-1.png')";
+	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-3/prison-1-1.png')";}, delay4);
+	setTimeout(function() {document.getElementById("game-container").style.background = "url('img/scenes/scene-3/prison-1-2.png')";}, delay4+=delay4time);
+
+	document.getElementById('button1pt2').id= 'btn1prison';
+	document.getElementById('button2pt02').id= 'btn2prison';
+	document.getElementById('button3pt2').id= 'btn3prison';
+
+	setTimeout(function() {document.getElementById('btn1prison').style.visibility = 'visible';}, delay4);
+	setTimeout(function() {document.getElementById('btn2prison').style.visibility = 'visible';}, delay4);
+	setTimeout(function() {document.getElementById('btn3prison').style.visibility = 'visible';}, delay4);
+}
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+function defeat() {
+	document.getElementById("game-container").style.background = "url('img/scenes/defeat/defeat 1.0.png')";
+	document.getElementById("button2pt02").style.background = "url('img/buttons/defeat-button.png')";
+	document.getElementById('button2pt02').style.visibility = 'visible';
 }
 
 
@@ -104,4 +156,6 @@ function s02button3clicked() {
 
 
 //document.getElementById("game-container").style.background = "url('img/scenes/intro-officer-stick-s2.PNG')";
+
+
 
